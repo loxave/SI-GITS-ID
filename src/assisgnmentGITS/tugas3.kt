075fde3a92956d57a1a4a1fd51bdaaa33e0cac44
ffrescott <zenfany54@gmail.com>
2021-09-07 14:59:46 +0700

    Menambah Single & Multiple Comment di tugas 3

--- src/assisgnmentGITS/tugas3.kt
+++ src/assisgnmentGITS/tugas3.kt
@@ -4,6 +4,11 @@ import java.util.*
 
 fun main() {
 
+    //Program sederhana menggunakan while untuk mencetak output
+    //mendapatkan tv atau sepeda
+    //berdasarkan nomor absen anak, dan memberikan kondisi
+    // apakah absen bernilai ganjil atau genap
+
 
     val inputScanner = Scanner(System.`in`)
     val kelasA: Int
@@ -12,9 +17,22 @@ fun main() {
     print("Jumlah Anak: ")
     kelasA = inputScanner.nextInt()
     println(kelasA)
-    
+
+    /**
+    Menggunakan while karena, saat variable anak banding value = 49
+    akan loop karena variable anak lebih kecil dibanding value = 49
+    dan ketika lebih kecil dari value, akan berjalan ke post increment pada variable
+    anak (anak++), saat turun ke blok selanjutnya, akan diberikan kondisi, ketika
+    anak yang memiliki absen genap akan mencetak output:
+    Untuk Anak-anak absen GENAP $anak mendapat TV
+    dan turun di blok selanjutnya, ketika kondisinya anak yang memiliki absen ganjil
+    akan mencetak output:
+    Untuk Anak-anak absen GANJIL $anak mendapat sepeda
+    ketika sudah tidak ada lagi yang diberikan kondisi, akan ada output default.
+    finish
+     **/
     while (anak <= 49) {
-        ++anak
+        anak++
         if (anak % 2 == 0) {
             println("Untuk Anak-anak absen GENAP $anak mendapat TV")
             continue
